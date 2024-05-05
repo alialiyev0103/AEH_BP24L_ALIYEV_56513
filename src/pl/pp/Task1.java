70d738701e56a61e6058017cd65d9cf5db82ec06
ali <alialiyev0103@gmail.com>
2024-05-05 23:46:46 +0200

    update

--- src/pl/pp/Task1.java
+++ src/pl/pp/Task1.java
@@ -1,28 +1,73 @@
-import java.util.Scanner;
+package pl.pp;
 
 public class Task1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.print("Enter lower and upper integer limits: ");
-            int lower = scanner.nextInt();
-            int upper = scanner.nextInt();
+        System.out.println("Calculations and display of the result for values assigned in the application code");
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-            if (upper <= lower) {
-                System.out.println("Done");
-                break;
-            }
-
-            int sum = 0;
-            for (int i = lower; i <= upper; i++) {
-                sum += i * i;
-            }
-
-            System.out.println("The sums of the squares from " + (lower * lower) + " to " + (upper * upper) + " is " + sum);
-            System.out.print("Enter next set of limits (or enter lower limit <= upper limit to quit): ");
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your score #1 is " + finalScore);
         }
 
-        scanner.close();
+        System.out.println("Calculations and display of the result for values assigned in the application code (different than before)");
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your score #2 is " + finalScore);
+        }
+
+        System.out.println("Calculations using the method calculateScore_noArguments(), but still for values assigned in the method code");
+        calculateScore_noArguments();
+
+        System.out.println("Calculations using the method calculateScore(), but this time for values entered into the method call argument");
+        calculateScore_arguments(true, 2500, 9, 250);
+        calculateScore_arguments(gameOver, score, levelCompleted, bonus);
+
+        System.out.println("Calculations and display can be made independent from each other by adding a parameter that this method will return using return");
+        finalScore = calculateScore_argumentsReturn(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your score #5 is " + finalScore);
+    }
+
+    private static void calculateScore_noArguments() {
+        boolean gameOver = true;
+        int score = 150;
+        int levelCompleted = 2;
+        int bonus = 1000;
+
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your score #3 is " + finalScore);
+        }
+    }
+    private static void calculateScore_arguments(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your score #4 is " + finalScore);
+        }
+    }
+    private static int calculateScore_argumentsReturn(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+        }
+        return finalScore;
     }
 }
--- src/pl/pp/Task2.java
+++ src/pl/pp/Task2.java
@@ -1,83 +1,17 @@
-import java.util.Scanner;
+package pl.pp;
 
 public class Task2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean continueCalculation = true;
 
-        while (continueCalculation) {
-            System.out.println("Menu:");
-            System.out.println("1. Add");
-            System.out.println("2. Subtract");
-            System.out.println("3. Multiply");
-            System.out.println("4. Divide");
-            System.out.println("5. Quit");
-            System.out.print("Enter your choice: ");
+        printCharacter('*', 5, 3);
+    }
 
-            int choice;
-            while (true) {
-                try {
-                    choice = Integer.parseInt(scanner.nextLine());
-                    if (choice >= 1 && choice <= 5) {
-                        break;
-                    } else {
-                        System.out.print("Invalid choice. Please enter a number between 1 and 5: ");
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.print("Invalid choice. Please enter a number between 1 and 5: ");
-                }
+    private static void printCharacter(char character, int timesInRow, int lines) {
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < timesInRow; j++) {
+                System.out.print(character);
             }
-
-            if (choice == 5) {
-                System.out.println("Goodbye!");
-                break;
-            }
-
-            double num1, num2;
-            System.out.print("Enter first number: ");
-            while (true) {
-                try {
-                    num1 = Double.parseDouble(scanner.nextLine());
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.print("Invalid input. Please enter (valid input) a number: ");
-                }
-            }
-
-            System.out.print("Enter second number: ");
-            while (true) {
-                try {
-                    num2 = Double.parseDouble(scanner.nextLine());
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.print("Invalid input. Please enter (valid input) a number: ");
-                }
-            }
-
-            double result = 0;
-            switch (choice) {
-                case 1:
-                    result = num1 + num2;
-                    break;
-                case 2:
-                    result = num1 - num2;
-                    break;
-                case 3:
-                    result = num1 * num2;
-                    break;
-                case 4:
-                    if (num2 != 0) {
-                        result = num1 / num2;
-                    } else {
-                        System.out.println("Logicall Error: Division by zero!");
-                        continue;
-                    }
-                    break;
-            }
-
-            System.out.println("Result: " + result);
+            System.out.println();
         }
-
-        scanner.close();
     }
 }
