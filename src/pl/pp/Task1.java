fa3041338be9180bc06c225a2d54b7adc170dde5
ali <alialiyev0103@gmail.com>
2024-05-05 23:37:48 +0200

    update

--- src/pl/pp/Task1.java
+++ src/pl/pp/Task1.java
@@ -1,6 +1,6 @@
 package pl.pp;
 public class Task1{
     public static void main(String[] args) {
-        System.out.println("Hello Basics of Programming!");
+        System.out.println("Helloo Basics of Programming!");
     }
 }
